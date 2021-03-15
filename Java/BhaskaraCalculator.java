b65c49d756479cdfa407b78a7311a876b311962a
Henrique Poleselo <hpoleselo@gmail.com>
2021-03-15 19:24:18 -0300

    Bhaskara calculator working, just have to understand the Handling Exception and scope declaration better.

--- Java/BhaskaraCalculator.java
+++ Java/BhaskaraCalculator.java
@@ -1,18 +1,45 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class BhaskaraCalculator {
     // Class method, can we change the name or it has to be main?
     public static void main(String args[]) {
+      double delta, x1, x2, a, b, c;
+
       // Instantiating the Scanner object, which will be called to read the variable from the user
       Scanner scanObj = new Scanner(System.in);
       System.out.println("Enter the coefficient a: ");
+
       // Read user input and store it to a variable
-      int a = scanObj.nextInt();
+      // Double input on the console HAS TO BE using comma, otherwise an exception will be thrown: 2,0 and not 2.0
+      //try {
+      a = scanObj.nextDouble();
       System.out.println("Enter the coefficient b: ");
-      int b = scanObj.nextInt();
+      b = scanObj.nextDouble();
       System.out.println("Enter the coefficient c: ");
-      int c = scanObj.nextInt();
-      System.out.printf("Coefficients: a = %d, b = %d and c = %d \n", a, b, c);
-      
+      c = scanObj.nextDouble();
+      //}
+
+      /*catch(java.util.InputMismatchException e) {
+        System.out.println("Use comma instead of points or integers! Exception caught: " + e);
+      }
+      */
+
+      System.out.printf("\nQuadratic equation is: %f*x^2 %fx + %f = 0\n", a, b, c);
+
+      delta = Math.pow(b,2) - 4*a*c;
+
+      if (delta >= 0) {
+        System.out.println("Delta: " + delta);
+        x1 = (-b + Math.sqrt(delta))/(2*a);
+        x2 = (-b - Math.sqrt(delta))/(2*a);
+        System.out.printf("Roots are: %f and %f \n", x1, x2);
+      }
+
+      else {
+        System.out.println("\nThe given coefficients results on imaginary roots. Exiting program...");
+        System.exit(0);
+      }
+
     }
 }
